e255db37876d1aa0caa4ee54b6771c1511cd787b
priv2024 <priv2024@tutanota.com>
2024-03-14 22:44:52 +0100

    add aliyun ak credential expressions

--- patterns/src/gen.rs
+++ patterns/src/gen.rs
@@ -1,5 +1,7 @@
 pub const PATTERNS: &'static [(&'static str, &'static str)] = &[
 ("aws::secret_access_key", r#"\<([0-9a-zA-Z+/]{40})\>"#),
 ("aws::access_key_id", r#"\<((?:A3T[A-Z0-9]|AKIA|AGPA|AROA|AIPA|ANPA|ANVA|ASIA)[A-Z0-9]{16})\>"#),
+("aliyun::access_key_secret", r#"\<([0-9a-zA-Z]{30})\>"#),
+("aliyun::access_key_id", r#"\<(LTAI[0-9a-zA-Z]{20})\>"#),
 ("_::email_address", r#"\<((?:[a-z0-9]+\.)*[a-z0-9]+@(?:[a-z0-9]+(?:\.|-))*[a-z0-9]+\.[a-z0-9]{2,10})\>"#),
 ];
